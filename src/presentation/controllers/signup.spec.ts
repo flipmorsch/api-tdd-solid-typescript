360ff9b8a901410ae6da32503240e5d9fdb66f18
Felipe Marques <flipmorsch@gmail.com>
2021-08-22 11:44:39 -0300

    fix: ensure SignUpController implements an HTTP interface

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,15 +3,33 @@ import { SignUpController } from './signup';
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
     const sut = new SignUpController();
+
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@email.com',
         password: 'nan44aocw',
         passwordConfirmation: 'nan44aocw',
       },
     };
+
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+  });
+
+  test('should return 400 if no email is provided', () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'nan44aocw',
+        passwordConfirmation: 'nan44aocw',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
   });
 });
